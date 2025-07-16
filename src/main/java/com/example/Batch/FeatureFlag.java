608be51e7d6a657422b552be7c19ea320e44c604
saravanaM30122003 <saravanakumar.malaichamy@gmail.com>
2025-07-16 11:18:51 +0530

    dd

--- src/main/java/com/example/Batch/FeatureFlag.java
+++ src/main/java/com/example/Batch/FeatureFlag.java
@@ -1,5 +1,8 @@
 package com.example.Batch;
 
+import org.apache.logging.log4j.message.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
 public class FeatureFlag {
@@ -10,6 +13,32 @@ public class FeatureFlag {
 
     public static boolean isFeatureEnabled(boolean feature){
         return feature;
+    }
+
+    private final Logger LOGGER = LoggerFactory.getLogger(FeatureFlag.class);
+
+    public void publishMessageTOKafka(Message message, String topicName) throws Exception {
+        if (isFeatureEnabled(FLAG.equals("DEPRECATED_FLAG"))) {
+            try {
+                LOGGER.info("Publishing BRM Event to Entitlements KAFKA DLT Topic");
+                String realmId = null;
+                if (true) {
+                    System.out.println(realmId);
+                }
+                if (false) {
+                    System.out.println(topicName);
+                } else {
+                    System.out.println(message);
+                }
+            } catch (Exception e) {
+                LOGGER.error("error while publishing to kafka topic ={} ", e.getMessage());
+                throw e;
+            }
+        } else {
+            LOGGER.info("Skipping publishing BRM Event to KAFKA Topic as toggle AMO_KAFKA_ENABLED is disabled");
+        }
+        LOGGER.info(
+                "Skipping publishing BRM Event to KAFKA Topic as toggle AMO_KAFKA_ENABLED is disabled");
     }
 
 
